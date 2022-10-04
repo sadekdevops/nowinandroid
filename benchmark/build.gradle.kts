58da11f46b003f20061ce4aff76527406a48df4d
Ben Weiss <benweiss@google.com>
2022-10-04 11:26:09 +0100

    New baseline profile
    Change-Id: I8a20ab2f44b198afaf57f26d1bee20054ed1ce6d

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -55,7 +55,7 @@ android {
     testOptions {
         managedDevices {
             devices {
-                maybeCreate<ManagedVirtualDevice>("pixel5Api32").apply {
+                create<ManagedVirtualDevice>("pixel5Api30").apply {
                     device = "Pixel 5"
                     apiLevel = 30
                     systemImageSource = "aosp"
